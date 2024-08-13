3f04aecfd5d2ffa9d98b2638191a66caec50c8c8
Michael Spranger <michael.spranger@quantinuum.com>
2024-08-13 18:53:21 +0100

    pybind11 update

---
+++ build_wheels.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -u -x
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/wheelhouse/
+    fi
+}
+
+PYBIN="/opt/python/cp311-cp311/bin"
+"${PYBIN}/pip" install -r /io/requirements-dev.txt
+"${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
